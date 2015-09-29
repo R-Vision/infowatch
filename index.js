6f766e284d32c3ff8f7ffdc22edf04df29705f29
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2015-09-29 13:54:26 +0300

    exports

--- index.js
+++ index.js
@@ -97,26 +97,4 @@ Infowatch.prototype.disconnect = function () {
     });
 };
 
-var options = {
-    user: "iwdm",
-    password: "xxXX1234",
-    connectString: "192.168.23.150/iwtm"
-};
-
-var infowatch = new Infowatch(options);
-
-infowatch
-    .connect()
-    .then(function () {
-        return infowatch.getHosts();
-    })
-    .map(function (host) {
-        host.users = infowatch.getUsers(host.id);
-        return Bluebird.props(host);
-    }, {concurrency: 1})
-    .then(function (hosts) {
-        console.log(hosts);
-    })
-    .finally(function () {
-        return infowatch.disconnect();
-    });
+module.exports = Infowatch;
